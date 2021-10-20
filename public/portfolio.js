9c5a5f3951e838d31030e7283f0b850eb00bdb72
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-10-20 13:12:36 -0500

    fixed the contact me icons

--- public/portfolio.js
+++ public/portfolio.js
@@ -1,1 +1,8 @@
- 
+var frmvalidator = new Validator("contactform");
+frmvalidator.addValidation("name", "req", "Please provide your name");
+frmvalidator.addValidation("email", "req", "Please provide your email");
+frmvalidator.addValidation(
+  "email",
+  "email",
+  "Please enter a valid email address"
+);
