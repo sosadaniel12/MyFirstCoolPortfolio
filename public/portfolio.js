1c5852cf1809b2d1c80ab85a6e4728b8bb6d0bd8
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-10-22 15:53:21 -0500

    fixed contact me form

--- public/portfolio.js
+++ public/portfolio.js
@@ -1,8 +1,23 @@
-var frmvalidator = new Validator("contactform");
-frmvalidator.addValidation("name", "req", "Please provide your name");
-frmvalidator.addValidation("email", "req", "Please provide your email");
-frmvalidator.addValidation(
-  "email",
-  "email",
-  "Please enter a valid email address"
-);
+var form = document.getElementById("form");
+form.addEventListener("submit", formSubmit);
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData();
+  formData.append(
+    "name",
+    document.querySelector('input[name="Contact-Name"]').value
+  );
+  formData.append(
+    "email",
+    document.querySelector('input[name="Contact-Email"]').value
+  );
+
+  fetch("https://getform.io/f/a103f70f-5192-4a75-a809-fc0e6cc87c45", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => console.log(response))
+    .catch((error) => console.log(error));
+}
