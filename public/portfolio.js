a09256ad6fcfd5540875fbf7701ba6fe8bb6339e
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-12-01 11:38:19 -0600

    changed caption

--- public/portfolio.js
+++ public/portfolio.js
@@ -14,9 +14,9 @@ $(window).on("load", function () {
           }
         } else {
           //object goes out of view (scrolling up)
-          if ($(this).css("opacity") == 1) {
-            $(this).easeIn(300, 0);
-          }
+          // if ($(this).css("opacity") == 1) {
+          //   $(this).easeIn(300, 0);
+          // }
         }
       });
     })
